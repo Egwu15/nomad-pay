f72eb4cbac8ec12f7821dfe4bb64ac73be237926
Egwu15 <egwutedd@gmail.com>
2023-08-10 12:50:02 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <HomePage />
+      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="bg-custom-blue text-white p-16 md:p-28 flex flex-col md:flex-row">
+      <div className="w-full md:w-2/6">
+        <p className="font-bold text-2xl">NomadPay</p>
+        <p className="pt-5">© 2023 NomadPay. All Rights Reserved</p>
+      </div>
+      <div className="w-full md:w-5/6 flex justify-end ">
+        <div className="grid-rows-3 gap-5 flex flex-col md:flex-row">
+          <div>
+            <h5 className="font-bold pb-3">Product</h5>
+            <p className="text-gray-500 text-sm pb-2">Money Transfer</p>
+            <p className="text-gray-500 text-sm pb-2">Currency Exchange</p>
+            <p className="text-gray-500 text-sm pb-2">Virtual Cards</p>
+          </div>
+          <div>
+            <h5 className="font-bold pb-3">Company</h5>
+            <p className="text-gray-500 text-sm pb-2">About</p>
+            <p className="text-gray-500 text-sm pb-2">Press</p>
+            <p className="text-gray-500 text-sm pb-2">Career</p>
+            <p className="text-gray-500 text-sm pb-2">Teams</p>
+            <p className="text-gray-500 text-sm pb-2">Blog</p>
+          </div>
+          <div>
+            <h5 className="font-bold pb-3">Resources</h5>
+            <p className="text-gray-500 text-sm pb-2">FAQs</p>
+            <p className="text-gray-500 text-sm pb-2">Contact Us</p>
+            <p className="text-gray-500 text-sm pb-2">Privacy</p>
+            <p className="text-gray-500 text-sm pb-2">Terms of Use</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
